8f29b17ef06e57a1df75c152d4f3faf74a73fbb7
ayunitiasarii <114816549+ayunitiasarii@users.noreply.github.com>
2023-12-13 06:53:52 +0000

    first commit

--- Hello.py
+++ Hello.py
@@ -1,51 +1,49 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import numpy as np
+import pandas as pd
 import streamlit as st
-from streamlit.logger import get_logger
+import tensorflow as tf
+from PIL import Image
 
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+"""
+Allergy Detection.
 
 
-if __name__ == "__main__":
-    run()
+This is demonstration of my CNN Model for detecting which the skin have allergy or not.
+The model is not train for detecting what kind of allergy is.
+
+The model need a picture with full (atleast 80%) of skin image.
+"""
+
+# Load trained model
+loaded_model = tf.keras.models.load_model('model.h5')
+input_shape = (128,128)
+label = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer as a PIL Image:
+    img = Image.open(img_file_buffer)
+    img_array = np.array(img)
+
+    image = Image.open(img_file_buffer)
+    # Resize img to fit in model input shape
+    image = image.resize(input_shape)
+    # Convert the image to a NumPy array
+    image_array = np.array(image)
+    image_array = image_array[:, :, :3]  # Keep only the first 3 channels (RGB)
+    # Normalize the pixel values
+    image_array = image_array / 255.0
+    # Expand dimensions to match the input shape dimension of the model
+    input_image = np.expand_dims(image_array, axis=0)
+
+    predictions = loaded_model.predict(input_image).tolist()[0]
+    predicted_label = np.argmax(predictions, axis=-1)
+    
+    st.write("Result:")
+    st.write(f"{100*predictions[predicted_label]:.2f}%",label[predicted_label])
+
+    st.write(pd.DataFrame({
+        'Label': label,
+        'Result': predictions,
+    }))
